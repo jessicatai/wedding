c1044f5b55993bd5e48412ea3c1f3eaabf1bda26
Jessica Tai <jessica.tai@airbnb.com>
2016-01-30 01:07:35 -0800

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = '80da0c363b8e684f3f8070f1f7a51b355ca858d46208eba0ed379b309a5fd64002d7fab5c1e91e62c883984077cedc1bf9d53d6177c21f9e3783fe365f7069a2'
