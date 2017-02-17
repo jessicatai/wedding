6dfc7516206fecd784749bac8c8fce61acf7be37
Jessica Tai <jessica.tai@airbnb.com>
2017-02-16 22:13:41 -0800

    Whitelist fields for user group and user read and write

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -1,4 +1,5 @@
 class RsvpController < ApplicationController
+  include ::Rsvp::Constants
   def show
     code = params[:code]
     @user_group = UserGroup.find_by_code(code)
@@ -9,31 +10,36 @@ class RsvpController < ApplicationController
       # format.html { render :action => :update }
       format.json {
         render :json => {
-          :user_group => @user_group,
-          :users => @user_group.users.as_json(:except => [:relationship]),
+          :user_group => @user_group.as_json(:only => USER_GROUP_PUBLIC_READ_FIELDS),
+          :users => @user_group.users.as_json(:only => USER_PUBLIC_READ_FIELDS),
         }
       }
     end
   end
 
+  respond_to :json
   def update
     id = params[:id]
     @user_group = UserGroup.find(id)
+    user_group_params = params[:user_group]
+
+    user_group_public_write_params = user_group_params.symbolize_keys
+      .slice(*USER_GROUP_PUBLIC_WRITE_FIELDS)
 
     # Saves user_group and users
-    if @user_group && @user_group.update_attributes(params[:user_group])
+    if @user_group && @user_group.update_attributes(user_group_public_write_params)
       params[:users] && params[:users].each do |id, attributes|
         user = User.find(id)
-        unless user.update_attributes!(attributes.except(:id))
+        unless user.update_attributes!(attributes.symbolize_keys.slice(*USER_PUBLIC_WRITE_FIELDS))
           Rails.logger.info("ERROR: Failed to update user #{user.id}")
         end
       end
-        render :json => {
-          :user_group => @user_group,
-          :users => @user_group.users.as_json(:except => [:relationship]),
+      render :json => {
+        :user_group => @user_group.as_json(:only => USER_GROUP_PUBLIC_READ_FIELDS),
+        :users => @user_group.users.as_json(:only => USER_PUBLIC_READ_FIELDS),
       }
     else
-      format.json { render :json => { :errors => "Oops, we couldn't update your info" } }
+      render :json => { :errors => "Oops, we couldn't update your info" }
     end
   end
 end
